7829947acfa83ce74c6c6968549237d01ec1fbc7
harmsso <harms_sophie@web.de>
2023-01-11 13:27:27 +0100

    correct sprite rotation & improved prefabs

--- Assets/Interactables/RotateSpriteToCamera.cs
+++ Assets/Interactables/RotateSpriteToCamera.cs
@@ -11,26 +11,6 @@ public class RotateSpriteToCamera : MonoBehaviour
 
     void Update()
     {
-        // Vector3 camera = main.position;
-        //
-        // var direction = camera - transform.position;
-        //
-        // float angle = Vector3.Angle(direction, transform.position);
-        //
-        // float clampedAngle = Mathf.Clamp(angle, 0, 45);
-        //
-        // Quaternion rotation = Quaternion.Euler(clampedAngle, 0, 0);
-        //
-        // transform.SetPositionAndRotation(transform.position, rotation);
-
-        var cameraRot = main.rotation;
-
-        // float angle = Vector3.Angle(direction, transform.position);
-        //
-        // float clampedAngle = Mathf.Clamp(angle, 0, 90);
-        //
-        // Quaternion rotation = Quaternion.Euler(clampedAngle, 0, 0);
-        //
-        // transform.SetPositionAndRotation(transform.position, rotation);
+        transform.rotation = main.rotation;
     }
 }
